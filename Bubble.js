5b91289d3185c1c0335983968620dff653176b60
Nikhil Chaudhary <150070913+nikhilchaudhary7108@users.noreply.github.com>
2024-03-14 14:26:06 +0530

    Create Bubble.js

---
+++ Bubble.js
@@ -0,0 +1,14 @@
+function bubbleSort(arr) {
+    const n = arr.length;
+    for (let i = 0; i < n; i++) {
+        for (let j = 0; j < n - i - 1; j++) {
+            if (arr[j] > arr[j+1]) {
+                [arr[j], arr[j+1]] = [arr[j+1], arr[j]];
+            }
+        }
+    }
+}
+
+const arr = [10, 7, 8, 9, 1, 5];
+bubbleSort(arr);
+console.log(arr.join(' '));
